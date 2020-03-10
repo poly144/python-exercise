7b08e0d6a701b5d0bb63d20089705efc4a9773a3
poly144 <ashleympoentle14@gmail.com>
2020-03-10 16:00:16 +0200

    poly.py

--- poly.py
+++ poly.py
@@ -1,12 +1,30 @@
-a = 3
-b = 2
-c = 1
-import math
+strSentence = "lifechoices coding academy"
+print(strSentence[2:])
+number1 = 25
+number2 = 50.5
+number3 = 3e+7j
+print(type(number1))
+print(type(number2))
+print(type(number3))
+#n1 was a integer..n2 was a float..n3 was a complex
+mylist = [65,25,125,78,120,33]
+print(type(mylist))
+#this was a list
+mylist.sort()
+print(mylist)
+#.sort sorted the lin in ascendind
+mylist.insert(2,27)
+print(mylist)
+#.insert 2,27 inserted 27 in space number2 in a developer language(number3 in normal)
+cars = ("datsun","ford","bmw","vw")
+print(cars)
+print(type(cars))
+#this was a tuple
+mylist.reverse()
+print(mylist)
+#.reverse changed the order from ascending to descending
+pdict = {"1":"any","2":"poly","3":"brentle"}
+print(type(pdict))
+#this was a dictionary
 
-b = b ** b
-print(b)
-c = c ** c
-print(c)
 
-x = math.sqrt(a - 3) / (b+c)
-print(x)
