313539c4d45a362b8fd6a397377688051de5469b
poly144 <ashleympoentle14@gmail.com>
2020-03-13 16:00:34 +0200

    this is a practice on a program to grade students

--- poly.py
+++ poly.py
@@ -1,6 +1,12 @@
-a = 0b10111011
-a >> 3
-print(bin(a >> 3))
-a = 0b10111011
-print(bin(~a))
+first_name = (input('student name:'))
+surname = (input('student surname:'))
+exam_mark = int(input('exam_mark:'))
 
+if (exam_mark <= 100)and(exam_mark >= 80):
+    print(first_name,surname,"oustanding")
+elif (exam_mark <= 79)and(exam_mark >= 60):
+    print(first_name, surname, "Satisfactory")
+elif(exam_mark <= 59)and(exam_mark >= 50):
+    print(first_name, surname, "Pass")
+elif(exam_mark <= 49)and(exam_mark >= 40):
+    print(first_name, surname, "Fail")
